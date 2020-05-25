c84b214794bf7dc5bfad1a784f17b9312e7c83a1
ankur mishra <ankur1999.mishra@gmail.com>
2020-05-24 19:22:32 -0700

    there is ju8st a Google sign in button added

--- app/src/main/java/com/example/yolodate/MainActivity.java
+++ app/src/main/java/com/example/yolodate/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.yolodate;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
