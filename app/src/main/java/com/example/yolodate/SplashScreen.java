c19b31328eb4ac117abef6b9c53eea2a4ca00b9c
ankur mishra <ankur1999.mishra@gmail.com>
2020-05-26 17:52:45 -0700

    did a lot more things today

---
+++ app/src/main/java/com/example/yolodate/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.yolodate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        image = findViewById(R.id.imageView6);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
