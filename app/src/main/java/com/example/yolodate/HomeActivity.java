c012ef8511e5f1f1e8dd55003be1faf582d83ec9
ankur mishra <ankur1999.mishra@gmail.com>
2020-05-27 19:33:12 -0700

    test

--- app/src/main/java/com/example/yolodate/HomeActivity.java
+++ app/src/main/java/com/example/yolodate/HomeActivity.java
@@ -16,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        //super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnLogout = findViewById(R.id.logout);
 
