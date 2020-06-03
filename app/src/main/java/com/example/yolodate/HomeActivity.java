ec6a1f2666e2f678398abb5f868d3c80289ddf77
ankur mishra <ankur1999.mishra@gmail.com>
2020-06-02 20:03:32 -0700

    Made some more new changes 😁😁

--- app/src/main/java/com/example/yolodate/HomeActivity.java
+++ app/src/main/java/com/example/yolodate/HomeActivity.java
@@ -29,7 +29,7 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
-    //protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnLogout = findViewById(R.id.logout);
