24f6427944860bc21c035ae145b968da043a7630
ankur mishra <ankur1999.mishra@gmail.com>
2020-06-05 14:43:46 -0700

    Made some more new changes

--- app/src/main/java/com/example/yolodate/User.java
+++ app/src/main/java/com/example/yolodate/User.java
@@ -1,23 +1,22 @@
 package com.example.yolodate;
 
-import com.example.yolodate.HomeActivity;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User extends HomeActivity {
-    ArrayList <Integer> traits = new ArrayList();
-    ArrayList <Integer> response = new ArrayList();
-    public User() {
-        traits = null;
-        response = null;
-    };
+    //int[] traits = new int[50];
+    int[] response = new int[50];
 
-    public void Quiz () {
+    public User() {
+        //traits = null;
+        //response = null;
+    }
+
+    public void Quiz() {
         Scanner resp = new Scanner(System.in);
         System.out.println("What is your gender?");
-        int response = resp.nextInt();
-        traits.add (response);
+        for (int i = 0; i < 50; i++) {
+            response[i] = resp.nextInt();
+        }
     }
 
     public static void main(String[] args) {
