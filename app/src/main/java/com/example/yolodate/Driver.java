bc3a861e4032bd26ddd3f1eabae0fe2dc5b206e0
nimdas3 <nimidas999@gmail.com>
2020-06-09 12:46:12 -0700

    User class updated

---
+++ app/src/main/java/com/example/yolodate/Driver.java
@@ -0,0 +1,59 @@
+package com.example.yolodate;
+
+public class Driver extends User{
+    double matchScore;  // Global variable which we are gonna use later
+
+    // This function determines if 2 users can be matched based on
+    // their gender and what sex they prefer
+    // We can try an array of users if this doesn't work on the app
+    static boolean MatchSex(User user1, User user2) {
+        boolean matchSuccess = false;					// Variable to determine if the users matched
+
+        // Get user1's info
+        String user1Gender = user1.getGender();
+        String user1PreferredSex = user1.getPreferredSex();
+
+        // Get user2's info
+        String user2Gender = user2.getGender();
+        String user2PreferredSex = user2.getPreferredSex();
+
+        // These if-else statements determine whether or not the match can be made
+        // based on the info the users provided
+        if ((user1Gender == user2PreferredSex) && (user2Gender == user1PreferredSex)) {
+            matchSuccess = true;
+        }
+        else if (user1PreferredSex == "BOTH" && (user1Gender == user2PreferredSex)) {
+            matchSuccess = true;
+        }
+        else if (user2PreferredSex == "BOTH" && (user2Gender == user1PreferredSex)) {
+            matchSuccess = true;
+        }
+        else {
+            matchSuccess = false;
+        }
+        return matchSuccess;
+    }
+
+    // Try the algorithm with 2 users
+    public static void main(String[] args) {
+        User me = new User();
+        me.setGender();
+        System.out.println(me.getGender());
+        me.setPreferredSex();
+        System.out.println(me.getPreferredSex());
+
+        User you = new User();
+        you.setGender();
+        System.out.println(you.getGender());
+        you.setPreferredSex();
+        System.out.println(you.getPreferredSex());
+
+        if (MatchSex(me,you) == true) {
+            System.out.println("Code to do rest of program");
+        }
+        else {
+            System.out.println("We will stop the program");
+        }
+    }
+
+}
--- app/src/main/java/com/example/yolodate/User.java
+++ app/src/main/java/com/example/yolodate/User.java
@@ -2,27 +2,80 @@ package com.example.yolodate;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User extends HomeActivity {
-    //int[] traits = new int[50];
-    int[] response = new int[100];
-    private DatabaseReference mDatabase;
+    private String gender;
+    private String preferredSex;
+    //private ArrayList<>
 
-    public User() {
-        //traits = null;
-        //response = null;
+    // Constructor: Initialize the instance variables to null
+    public User () {
+        gender = null;
+        preferredSex = null;
     }
 
-    public void Quiz() {
-        Scanner resp = new Scanner(System.in);
+    // This function asks the user their gender
+    // The user must enter 'a', 'b', or 'c' and its case insensitive
+    public void setGender() {
+        char response;
+        Scanner genderResponse = new Scanner(System.in);
         System.out.println("What is your gender?");
-        for (int i = 0; i < 100; i++) {
-            response[i] = resp.nextInt();
+        response = genderResponse.next().charAt(0);
+
+        // User entered invalid letter so we ask them again
+        while (!(response == 'A' || response == 'a' || response == 'B' || response == 'b' ||
+                response == 'C' || response == 'c' || response == 'D' || response == 'd')) {
+            System.out.println("You entered an invalid letter. Try again.");
+            response = genderResponse.next().charAt(0);
+        }
+
+        if (response == 'A' || response == 'a') {
+            gender = "MALE";
+        }
+        else if (response == 'B' || response == 'b') {
+            gender = "FEMALE";
+        }
+        else if (response == 'C' || response == 'c') {
+            gender = "OTHER";
         }
     }
 
-    public static void main(String[] args) {
-        
+    // This function returns the user's gender
+    public String getGender() {
+        return gender;
     }
+
+    // This function asks the user their preferred sex
+    // The user must enter 'a', 'b', or 'c' and its case insensitive
+    public void setPreferredSex() {
+        char response;
+        Scanner preferredSexResponse = new Scanner(System.in);
+        System.out.println("What is your preferred sex?");
+        response = preferredSexResponse.next().charAt(0);
+
+        // User entered invalid letter so we ask them again
+        while (!(response == 'A' || response == 'a' || response == 'B' || response == 'b' ||
+                response == 'C' || response == 'c' || response == 'D' || response == 'd')) {
+            System.out.println("You entered an invalid letter. Try again.");
+            response = preferredSexResponse.next().charAt(0);
+        }
+
+        if (response == 'A' || response == 'a') {
+            preferredSex = "MALE";
+        }
+        else if (response == 'B' || response == 'b') {
+            preferredSex = "FEMALE";
+        }
+        else if (response == 'C' || response == 'c') {
+            preferredSex = "BOTH";
+        }
+    }
+
+    // This function returns the user's preferred sex
+    public String getPreferredSex() {
+        return preferredSex;
+    }
+
 }
