7f0f16d9f683746883c2ac600fd67888ddc5c1bc
ankur mishra <ankur1999.mishra@gmail.com>
2020-06-10 21:17:44 -0700

    made some more awsome changes

--- app/src/main/java/com/example/yolodate/HomeActivity.java
+++ app/src/main/java/com/example/yolodate/HomeActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
@@ -20,6 +25,8 @@ public class HomeActivity extends AppCompatActivity {
     Button mOther;
     Button mNext;
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private String userId, male, female, other;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,10 @@ public class HomeActivity extends AppCompatActivity {
         mOther = findViewById(R.id.button5);
         mNext = findViewById(R.id.button);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        getUserInfo();
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,7 +75,6 @@ public class HomeActivity extends AppCompatActivity {
                 String other = mOther.getText().toString().trim();
                 HashMap<String, String> dataMap = new HashMap<>();
                 dataMap.put("Name", other);
-                mDatabase.push().setValue(dataMap);
             }
         });
         mNext.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +83,48 @@ public class HomeActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 Intent intToMain = new Intent(HomeActivity.this, Question2.class);
                 startActivity(intToMain);
+                saveUserInformation();
             }
+        });
+    }
+
+    private void saveUserInformation() {
+        male = mMale.getText().toString();
+        female = mFemale.getText().toString();
+        other = mOther.getText().toString();
+        Map userInfo = new HashMap();
+        userInfo.put("name", male);
+        userInfo.put("name", female);
+        userInfo.put("name", other);
+        mDatabase.updateChildren(userInfo);
+    }
+
+    private void getUserInfo() {
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if (map.get("name") != null) {
+                        male = map.get("name").toString();
+                        mMale.setText(male);
+                    }
+                    if (map.get("phone") != null) {
+                        female = map.get("phone").toString();
+                        mFemale.setText(female);
+                    }
+                    if (map.get("phone") != null) {
+                        other = map.get("phone").toString();
+                        mOther.setText(other);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
         });
     }
 }
